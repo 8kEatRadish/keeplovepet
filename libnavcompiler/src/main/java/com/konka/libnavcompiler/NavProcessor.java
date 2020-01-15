d224984f102cc0f480e684890ab5f7930cd2ee97
sniperking <18845795735@163.com>
2020-01-15 19:02:47 +0800

    feat: 未完待续

---
+++ libnavcompiler/src/main/java/com/konka/libnavcompiler/NavProcessor.java
@@ -0,0 +1,48 @@
+package com.konka.libnavcompiler;
+
+
+import com.google.auto.service.AutoService;
+import com.konka.libnavannotation.ActivityDestination;
+import com.konka.libnavannotation.FragmentDestination;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+
+@AutoService(Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes({"com.konka.libnavannotation.FragmentDestination","com.konka.libnavannotation.ActivityDestination"})
+public class NavProcessor extends AbstractProcessor {
+
+    private Messager messager;
+    private Filer filer;
+    private static final String OUTPUT_FILE_NAME = "destination.json";
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+        messager = processingEnvironment.getMessager();
+        filer = processingEnvironment.getFiler();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        Set<? extends Element> fragmentElements = roundEnvironment.getElementsAnnotatedWith(FragmentDestination.class);
+        Set<? extends Element> activityElements = roundEnvironment.getElementsAnnotatedWith(ActivityDestination.class);
+        if (!fragmentElements.isEmpty() || !activityElements.isEmpty()){
+            
+        }
+
+        return true;
+    }
+}
